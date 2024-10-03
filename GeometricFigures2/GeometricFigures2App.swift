42bfe09c14952a930a645badf9eb76f820188367
Ethan Chen <echen@lcs.on.ca>
2024-10-03 08:55:27 -0400

    Initial Commit

---
+++ GeometricFigures2/GeometricFigures2App.swift
@@ -0,0 +1,17 @@
+//
+//  GeometricFigures2App.swift
+//  GeometricFigures2
+//
+//  Created by Pak Ching Ethan Chen on 2024-10-03.
+//
+
+import SwiftUI
+
+@main
+struct GeometricFigures2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
